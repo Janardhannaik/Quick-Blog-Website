ef0d596d627cff835be830d993b3e8a00afa4f7d
JANARDHAN NAIK <naikjanardhan568@gmail.com>
2024-05-18 22:39:40 +0530

    routes added

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,11 @@ const express = require("express");
 const { default: mongoose } = require("mongoose");
 const app = express();
 const dotenv = require("dotenv");
+const authRoute = require("./routes/auth");
+const userRoute = require("./routes/users");
+const postRoute = require("./routes/posts");
+const commentRoute = require("./routes/comments");
+const cookieParser = require("cookie-parser");
 
 const connectDB = async () => {
   try {
@@ -14,6 +19,11 @@ const connectDB = async () => {
 
 //middlewares
 dotenv.config();
+app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
+app.use("/api/posts", postRoute);
+app.use("/api/comments", commentRoute);
+app.use(cookieParser());
 
 app.listen(process.env.PORT, () => {
   connectDB();
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,43 +1,78 @@
-import { Link } from "react-router-dom";
-import { ImSearch } from "react-icons/im";
+import { Link, useLocation, useNavigate } from "react-router-dom";
+import { BsSearch } from "react-icons/bs";
+import { FaBars } from "react-icons/fa";
+import { useContext, useState } from "react";
+import Menu from "./Menu";
+import { UserContext } from "../context/UserContext";
 
-function Navbar() {
-  const user = false;
+const Navbar = () => {
+  const [prompt, setPrompt] = useState("");
+  const [menu, setMenu] = useState(false);
+  const navigate = useNavigate();
+  const path = useLocation().pathname;
+
+  // console.log(prompt)
+
+  const showMenu = () => {
+    setMenu(!menu);
+  };
+
+  const { user } = useContext(UserContext);
+
   return (
     <div className="flex items-center justify-between px-6 md:px-[200px] py-4">
-      <h1 className="text-xl font-extrabold">
-        <Link to="/">CodSoft Blog</Link>
+      <h1 className="text-lg md:text-xl font-extrabold">
+        <Link to="/">Blog Market</Link>
       </h1>
-      <div className="flex justify-center items-center space-x-0">
-        <p>
-          <ImSearch />
-        </p>
-        <input
-          className="rounded-2xl px-6"
-          placeholder="Seach a post"
-          type="text"
-        />
-      </div>
-      <div className="flex items-center justify-center space-x-2 md:space-x-4">
+      {path === "/" && (
+        <div className="flex justify-center items-center space-x-0">
+          <p
+            onClick={() =>
+              navigate(prompt ? "?search=" + prompt : navigate("/"))
+            }
+            className="cursor-pointer"
+          >
+            <BsSearch />
+          </p>
+          <input
+            onChange={(e) => setPrompt(e.target.value)}
+            className="outline-none px-3 "
+            placeholder="Search a post"
+            type="text"
+          />
+        </div>
+      )}
+      <div className="hidden md:flex items-center justify-center space-x-2 md:space-x-4">
         {user ? (
           <h3>
-            <Link to="/create">Create</Link>
+            <Link to="/write">Write</Link>
           </h3>
         ) : (
           <h3>
-            <Link to="/login">login</Link>
+            <Link to="/login">Login</Link>
           </h3>
         )}
         {user ? (
-          <h3>Profile</h3>
+          <div onClick={showMenu}>
+            <p className="cursor-pointer relative">
+              <FaBars />
+            </p>
+            {menu && <Menu />}
+          </div>
         ) : (
-          <h3 className="text-[#000000] hover:text-[#6d61db]">
+          <h3>
             <Link to="/register">Register</Link>
           </h3>
         )}
       </div>
+      <div onClick={showMenu} className="md:hidden text-lg">
+        <p className="cursor-pointer relative">
+          <FaBars />
+        </p>
+        {menu && <Menu />}
+      </div>
     </div>
   );
-}
+};
 
 export default Navbar;
